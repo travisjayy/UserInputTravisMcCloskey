e350b6e6d7e0da965ec72cc7ad56ed17bb9c9003
Travis McCloskey <tmccloskey@usf.edu>
2020-01-31 21:27:12 -0500

    Add project files.

---
+++ UserInputTravisMcCloskey/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace UserInputTravisMcCloskey
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hi, this is Travis McCloskey :D, type something!");
+            string wordsPersonTyped;
+            wordsPersonTyped = Console.ReadLine();
+            Console.WriteLine("Are you sure you wanted to type: " + wordsPersonTyped + "?");
+        }
+    }
+}
